352b84d7c4b11d3097f50bede1661db6715611a6
Kevin Johnson <vadskye@gmail.com>
2017-10-11 23:29:30 +0000

    hide deprecated output

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 function main() {
   const sequelize = new Sequelize(
     process.env.DATABASE_URL,
-    {dialect: 'postgres'}
+    {dialect: 'postgres', operatorsAliases: false}
   );
 
   return sequelize.query(`select * from person limit 10`);
